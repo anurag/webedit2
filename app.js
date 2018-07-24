3d50a0266a6d58499ea5ef0a2973397d406910f5
cosmokay <cosmo@nerq.com>
2018-07-23 23:08:59 -0700

    es

--- app.js
+++ app.js
@@ -43,8 +43,8 @@ function updatecookietouid(snapshot) {
 }
 
 app.use((req, res, next) => {
-	console.log('vhost', req.vhost);
-	if (PORT === 80 && req.vhost === 'stage.nerq.com') res.redirect(req.vhost + ':8080');
+	console.log('vhost', req.get('Host'));
+	if (PORT === 80 && req.get('Host') === 'stage.nerq.com') res.redirect(req.get('Host') + ':8080');
 	next();
 });
 
